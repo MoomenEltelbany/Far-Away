7ef7da27628912608d60cba5bac1a1e985a9c310
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2025-01-09 01:52:39 +0200

    Finished the form style

--- far-away/src/App.js
+++ far-away/src/App.js
@@ -9,12 +9,21 @@ function App() {
                 <form>
                     <select>
                         <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
                     </select>
-                    <input type="text" placeholder="item" />
-                    <button type="submit"></button>
+                    <input type="text" placeholder="item..." />
+                    <button type="submit">Add</button>
                 </form>
             </section>
-            <main>
+            <main className="list-body">
                 <button>Clear list</button>
             </main>
             <footer>Start adding some items to your packing list</footer>
